985529d9d342ec29f841e87ece975d2c4e09f1e2
ebenezer <e.kankam95@hotmail.com>
2021-05-11 15:23:01 -0500

    added stripe payment functionality

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,9 @@ import Signup from "./pages/Signup";
 import Nav from "./components/Nav";
 import { StoreProvider } from "./utils/GlobalState";
 import OrderHistory from "./pages/OrderHistory";
+import Success from './pages/Success';
+
+
 
 const client = new ApolloClient({
   request: (operation) => {
@@ -37,6 +40,7 @@ function App() {
               <Route exact path="/signup" component={Signup} />
               <Route exact path="/orderHistory" component={OrderHistory} />
               <Route exact path="/products/:id" component={Detail} />
+              <Route exact path="/success" component={Success} />
               <Route component={NoMatch} />
             </Switch>
           </StoreProvider>
--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -5,9 +5,14 @@ import CartItem from "../CartItem";
 import Auth from "../../utils/auth";
 import { useStoreContext } from "../../utils/GlobalState";
 import "./style.css";
+import { QUERY_CHECKOUT } from '../../utils/queries';
+import { loadStripe } from '@stripe/stripe-js';
+import { useLazyQuery } from '@apollo/react-hooks';
 
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 const Cart = () => {
     const [state, dispatch] = useStoreContext();
+    const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
 
     function toggleCart() {
         dispatch({ type: TOGGLE_CART });
@@ -25,12 +30,33 @@ const Cart = () => {
         }
     }, [state.cart.length, dispatch]);
 
+    useEffect(() => {
+        if (data) {
+            stripePromise.then((res) => {
+                res.redirectToCheckout({ sessionId: data.checkout.session });
+            });
+        }
+    }, [data]);
+
     function calculateTotal() {
         let sum = 0;
         state.cart.forEach(item => {
             sum += item.price * item.purchaseQuantity;
         });
         return sum.toFixed(2);
+    }
+
+    function submitCheckout() {
+        const productIds = [];
+
+        state.cart.forEach((item) => {
+            for (let i = 0; i < item.purchaseQuantity; i++) {
+                productIds.push(item._id);
+            }
+        });
+        getCheckout({
+            variables: { products: productIds }
+        });
     }
 
     if (!state.cartOpen) {
@@ -59,9 +85,9 @@ const Cart = () => {
 
                         {
                             Auth.loggedIn() ?
-                                <button>
+                                <button onClick={submitCheckout}>
                                     Checkout
-              </button>
+</button>
                                 :
                                 <span>(log in to check out)</span>
                         }
