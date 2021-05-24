b00688092a564e6f9a27d6d2b7fd6bab37d83e26
ebenezer <e.kankam95@hotmail.com>
2021-05-23 21:53:59 -0500

    ran npm run build

---
+++ client/build/precache-manifest.ba4508b3226bd81cb1f4928aa7e10727.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72b7ab781ad55effe22484d7e0220136",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff5b2a179c02a45674b1",
+    "url": "/static/css/main.b38a3b8e.chunk.css"
+  },
+  {
+    "revision": "7ea1bbaddd9d286dfade",
+    "url": "/static/js/2.fbf554e7.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.fbf554e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff5b2a179c02a45674b1",
+    "url": "/static/js/main.e3858c79.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
